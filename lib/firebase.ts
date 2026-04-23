14ab94768418392f7f0eff6846254b564a8401bd
Daniel Castellanos Gallardo <danielito.cg.31@gmail.com>
2026-04-23 17:46:34 -0500

    variables

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,17 +3,32 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+const envFirebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+};
 
-// Your web app's Firebase configuration
+const missingEnv = Object.entries(envFirebaseConfig)
+  .filter(([, value]) => !value)
+  .map(([key]) => key);
+
+if (missingEnv.length > 0) {
+  throw new Error(
+    `Missing Firebase env vars: ${missingEnv.join(", ")}. Configure EXPO_PUBLIC_FIREBASE_* in your .env file.`,
+  );
+}
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBtR_EeuacsvEjmV12kzEMxuiJv7elw7rE",
-  authDomain: "sudostress.firebaseapp.com",
-  projectId: "sudostress",
-  storageBucket: "sudostress.firebasestorage.app",
-  messagingSenderId: "766572469279",
-  appId: "1:766572469279:web:5af01713bea6cd032d092a",
+  apiKey: envFirebaseConfig.apiKey as string,
+  authDomain: envFirebaseConfig.authDomain as string,
+  projectId: envFirebaseConfig.projectId as string,
+  storageBucket: envFirebaseConfig.storageBucket as string,
+  messagingSenderId: envFirebaseConfig.messagingSenderId as string,
+  appId: envFirebaseConfig.appId as string,
 };
 
 // Initialize Firebase
